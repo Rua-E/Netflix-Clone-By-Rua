cc3c62a52f1a7d978cc9a6eab33b9f1003ec2cd3
Rua <rua.elkasheef@gmail.com>
2024-05-02 18:11:31 -0400

    adding prisma schema

--- pages/auth.tsx
+++ pages/auth.tsx
@@ -1,5 +1,5 @@
 import { useCallback, useState } from "react";
-import  Input  from "./components/input";
+import  Input  from "../components/input";
 
 const Auth = () => {
     const [email, setEmail] = useState('');
